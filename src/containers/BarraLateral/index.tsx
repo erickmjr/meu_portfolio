c57c5c750895a26eb9334f90ef53e3d9c75c3020
erickmjr <erickmellojr@gmail.com>
2025-07-15 20:11:22 -0300

    feat: BarraLateral

---
+++ src/containers/BarraLateral/index.tsx
@@ -0,0 +1,11 @@
+import { BarraLateralStyled } from './styles';
+
+const BarraLateral = () => {
+    return (
+        <BarraLateralStyled>
+            <h1>@erickmjr</h1>
+        </BarraLateralStyled>
+    );
+};
+
+export default BarraLateral;
---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from 'styled-components';
+
+export const BarraLateralStyled = styled.aside`
+    background-color: red;
+    height: 100%;
+    padding: 2dvh;
+`;
